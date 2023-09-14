221c5f8cc9821cbcd2c49ecd730093fee2e8d242
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2023-09-14 11:47:02 -0400

    header added

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -16,7 +16,7 @@ export default function PokedexNavigation() {
       <Stack.Screen
         name="Pokemon"
         component={PokemonScreen}
-        options={{ title: "Pokemon" }}
+        options={{ title: "", headerTransparent: true }}
       />
     </Stack.Navigator>
   );
