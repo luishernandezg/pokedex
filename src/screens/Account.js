56fabad5ba2a0e9b579e63287b4b5648fff189d0
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2023-09-18 10:57:15 -0400

    added basic form

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,10 +1,9 @@
-import { SafeAreaView, View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View, Text } from "react-native";
+import React from "react";
+import LoginForm from "../components/auth/LoginForm";
+import UserData from "../components/auth/UserData";
 
 export default function Account() {
-  return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
-  )
+  const auth = null;
+  return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 }
